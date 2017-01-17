81379ce8b7e97445770769e81c8ce165460cf572
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2017-01-17 12:34:19 +0100

    Update ubuntu-setup.sh

--- deployment/ubuntu-setup.sh
+++ deployment/ubuntu-setup.sh
@@ -23,3 +23,4 @@ pip install docker-compose
 sudo apt-get install -y nfs-common
 wget https://github.com/ContainX/docker-volume-netshare/releases/download/v0.33/docker-volume-netshare_0.33_amd64.deb
 sudo dpkg -i docker-volume-netshare_0.33_amd64.deb
+sudo service docker-volume-netshare start
