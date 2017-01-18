5ac607cd4c0ce36b31d6542974ab747a8aff5b77
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2017-01-18 10:15:13 +0100

    Update start-cluster.sh

--- deployment/start-cluster.sh
+++ deployment/start-cluster.sh
@@ -16,18 +16,55 @@ MASTER_NODE="172.16.18.127"
 WORKER_NODES=("172.16.19.237", "172.16.19.197")
 JOIN_TOKEN=""
 
+NFS_MOUNT_DIR="/var/nfs/general"
+
 function setup_master_node() {
   echo "Setting up master at $MASTER_NODE...";
   JOIN_TOKEN=$(ssh $USERNAME@$MASTER_NODE 'docker swarm init &> /dev/null; docker swarm join-token manager -q >&1');
   echo "Join token is $JOIN_TOKEN";
   
-  echo "Starting NFS server..."
+}
+
+function setup_nfs_master() {
+  echo "Setting up NFS server..."
+  
+  ssh $USERNAME@$MASTER_NODE << EOF
+    # Create mounting point and make it non superuser accessible
+    sudo mkdir $NFS_MOUNT_DIR -p
+    sudo chown nobody:nogroup /var/nfs/general
+  
+    # Setup options in settings file 
+    nfs_options = ""
+    for worker in $WORKER_NODES; do
+      nfs_options = "$nfs_options\n$NFS_MOUNT_DIR $worker(rw,sync,no_subtree_check)"
+    done
+    sudo echo nfs_options > /etc/exports
+  
+    # Restart to make changes happen
+    sudo systemctl restart nfs-kernel-server
+  
+    # Setup firewall
+    for worker in $WORKER_NODES; do
+      sudo ufw allow from $worker to any port nfs
+    done
+  EOF
+  
+  echo "Done setting up NFS server..."
+}
+
+function setup_nfs_client() {
+  for worker in $WORKER_NODES; do
+      ssh $USERNAME@$worker << EOF
+        sudo mkdir -p $MOUNT_DIR
+        sudo mount $MASTER_NODE:$MOUNT_DIR $MOUNT_DIR
+      EOF
+  done
 }
 
 function setup_worker_node() {
   for worker in $WORKER_NODES; do
     echo "Setting up worker at $worker";
-    ssh $USERNAME@$MASTER_NODE "docker swarm join --token $JOIN_TOKEN $MASTER_NODE:$SWARM_PORT";
+    ssh $USERNAME@$worker "docker swarm join --token $JOIN_TOKEN $MASTER_NODE:$SWARM_PORT";
   done
 }
 
