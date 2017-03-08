34fa63c047b7f08ffe8b31121d1113a523f6a07c
Niklas Riekenbrauck <nikriek@gmail.com>
2017-03-08 15:21:45 +0100

    Optimize unique values

--- features/tasks.py
+++ features/tasks.py
@@ -69,9 +69,10 @@ def calculate_feature_statistics(feature_id):
     feature.max = np.amax(feature_col).item()
     feature.mean = np.mean(feature_col).item()
     feature.variance = np.nanvar(feature_col).item()
-    feature.is_categorical = (np.unique(feature_col).size < 10)
+    unique_values = np.unique(feature_col)
+    feature.is_categorical = (unique_values.size < 10)
     if feature.is_categorical:
-        feature.categories = list(np.unique(feature_col))
+        feature.categories = list(unique_values)
     feature.save(update_fields=['min', 'max', 'variance', 'mean', 'is_categorical', 'categories'])
 
 
